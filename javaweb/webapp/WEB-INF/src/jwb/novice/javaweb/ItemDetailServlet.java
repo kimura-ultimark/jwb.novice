891025ac5c59a07afacd095f4bd5763a14c6dae0
Y.Kimura <Y.Kimura@192.168.1.6>
2018-08-12 11:55:47 +0900

    サーブレット追加

---
+++ javaweb/webapp/WEB-INF/src/jwb/novice/javaweb/ItemDetailServlet.java
@@ -0,0 +1,17 @@
+package jwb.novice.javaweb;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ItemDetailServlet extends HttpServlet {
+	
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) 
+			throws ServletException, IOException {
+		
+	}
+}
